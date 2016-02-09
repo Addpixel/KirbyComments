41b799d5a1727311f388f1be4d1f4ae001829d4d
Florian Pircher <git@leuchtschirm.com>
2016-02-09 16:55:50 +0100

    Document Properties of `Comment`

--- comment.php
+++ comment.php
@@ -5,12 +5,50 @@
  */
 class Comment
 {
+  /**
+   * The per-page unique identifier of the comment. Ids start at 1 and not a 0.
+   *
+   * @var int
+   */
   private $id;
+  /**
+   * The name of the author of the comment.
+   *
+   * @var string
+   */
   private $name;
+  /**
+   * The e-mail address of the author of the comment. `null` if no e-mail
+   * address was provided.
+   *
+   * @var string
+   */
   private $email;
+  /**
+   * The address of the website of the author of the comment. `null` if no
+   * website address was provided.
+   *
+   * @var string
+   */
   private $website;
+  /**
+   * The message of the comment. May contain markdown-like formatting
+   * instructions.
+   *
+   * @var string
+   */
   private $message;
+  /**
+   * The point in time of when the comment was posted.
+   *
+   * @var \DateTime
+   */
   private $date;
+  /**
+   * Whether the comment is just a preview and has not been stored yet.
+   *
+   * @var bool
+   */
   private $is_preview;
   
   function __construct()
