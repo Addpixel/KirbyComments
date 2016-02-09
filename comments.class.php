4c477ef6f38819c828b806ec81b0aaf570c482e7
Florian Pircher <git@leuchtschirm.com>
2016-02-09 15:08:08 +0100

    Implement Iterator

--- comments.class.php
+++ comments.class.php
@@ -3,7 +3,46 @@
 /**
  * Comments
  */
-class Comments
+class Comments implements Iterator
 {
+  private $iterator_index;
+  private $comments;
+  private $stored_comments;
+  
+  function __construct($page)
+  {
+    $this->iterator_index = 0;
+    $this->comments = array();
+    $this->stored_comments = array();
+  }
+  
+  // ============
+  // = Iterator =
+  // ============
+  
+  function rewind()
+  {
+    $this->iterator_index = 0;
+  }
+  
+  function current()
+  {
+    return $this->comments[$this->iterator_index];
+  }
+  
+  function key()
+  {
+    return $this->iterator_index;
+  }
+  
+  function next()
+  {
+    $this->iterator_index += 1;
+  }
+  
+  function valide()
+  {
+    return isset($this->comments[$this->iterator_index]);
+  }
   
 }
