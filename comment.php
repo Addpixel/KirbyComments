45c58790e04d512a12c9b78ebad399ac19c46168
Florian Pircher <git@leuchtschirm.com>
2016-02-10 19:38:18 +0100

    Revert "Keep Tags in Messages as htmlspecialchars"
    This reverts commit dd610d60df0e2095da2fe91dc52008fadfff6311.

--- comment.php
+++ comment.php
@@ -72,7 +72,7 @@ class Comment
     $this->id         = $id;
     $this->name       = htmlspecialchars(trim(strip_tags($name)));
     $this->email      = htmlspecialchars(trim(strip_tags($email)));
-    $this->website    = strip_tags(htmlspecialchars(trim($website)));
+    $this->website    = htmlspecialchars(trim(strip_tags($website)));
     $this->message    = htmlspecialchars(trim(strip_tags($message)));
     $this->datetime   = $datetime;
     $this->is_preview = $is_preview === true;
