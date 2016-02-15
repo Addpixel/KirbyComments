d5a7760926b3bc935b96810704a79063a92e9f71
Florian Pircher <git@leuchtschirm.com>
2016-02-15 21:55:09 +0100

    Add Email Template

---
+++ email.template.php
@@ -0,0 +1,9 @@
+Hello {{ author }}!
+
+You've received a new comment by {{ comment.user.name }} on your page {{ page.name }} ({{ page.url }}).
+
+---
+{{ comment.message }}
+---
+
+Comment page: {{ comment_page.url }}
