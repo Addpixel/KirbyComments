c361e91c184da510f69c5a9bfbd297502fbb7e36
Florian Pircher <git@leuchtschirm.com>
2016-02-15 23:17:23 +0100

    Simplify Email Template

--- email.template.php
+++ email.template.php
@@ -1,9 +1,7 @@
-Hello {{ author }}!
+Hello!
 
 You've received a new comment by {{ comment.user.name }} on your page {{ page.name }} ({{ page.url }}).
 
 ---
 {{ comment.message }}
 ---
-
-Comment page: {{ comment_page.url }}
