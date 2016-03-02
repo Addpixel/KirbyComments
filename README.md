346bb465b98c908e034848eaad0a1fa21f888b32
Florian Pircher <git@leuchtschirm.com>
2016-03-02 20:45:03 +0100

    Check for long Username, Email Address and Website

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 | Domain | Code | Description |
 |---|---|---|
-| – | 0 | Success |
+| – | 0 | Success. |
 | System | 100 | ID must be of type `"int"`. |
 | System | 101 | ID must be greater than 0. |
 | System | 102 | Could not create `Comment` from page. |
@@ -14,8 +14,11 @@
 | Developer | 203 | Could not send email. |
 | User | 300 | Session is invalid. |
 | User | 301 | Name field must not be empty. |
-| User | 302 | Email field must not be empty. (Only when requiring email-address.) |
-| User | 303 | Email-address must be valid. (Only when requiring email-address.) |
-| User | 304 | Website field must not contain JavaScript code. |
-| User | 305 | Message field must not be empty. |
-| User | 306 | Message is to long. |
+| User | 302 | Name is too long. |
+| User | 303 | Email field must not be empty. (Only when requiring email-address.) |
+| User | 304 | Email-address must be valid. (Only when requiring email-address.) |
+| User | 305 | Email address is too long. |
+| User | 306 | Website field must not contain JavaScript code. |
+| User | 307 | Website address is too long. |
+| User | 308 | Message field must not be empty. |
+| User | 309 | Message is too long. |
--- comment.php
+++ comment.php
@@ -67,17 +67,22 @@ class Comment
       throw new Exception('The id of a comment must be bigger than 0.', 101);
     } elseif (trim($name) == '') {
       throw new Exception('The name field is required.', 301);
+    } elseif (strlen($name) > 64) {
+      throw new Exception('The name is too long.', 302);
     } elseif (Comments::option('require.email') && preg_match('/^\s*$/', $email)) {
-      throw new Exception('The e-mail address field is required.', 302);
+      throw new Exception('The email address field is required.', 303);
     } elseif (Comments::option('require.email') && !v::email($email)) {
-      throw new Exception('The e-mail address is not valid.', 303);
+      throw new Exception('The email address is not valid.', 304);
+    } elseif (strlen($email) > 64) {
+      throw new Exception('The email address is too long.', 305);
     } elseif (preg_match('/^\s*javascript:/i', $website)) {
-      throw new Exception('The website address may not contain JavaScript code.', 304);
+      throw new Exception('The website address may not contain JavaScript code.', 306);
+    } elseif (strlen($website) > 64) {
+      throw new Exception('The website address is too long.', 307);
     } elseif (trim($message) == '') {
-      throw new Exception('The message must not be empty.', 305);
+      throw new Exception('The message must not be empty.', 308);
     } elseif (strlen($message) > Comments::option('max_character_count')) {
-      var_dump($message, strlen($message));
-      throw new Exception('The message is to long. (A maximum of '.Comments::option('max_character_count').' characters is allowed.)', 306);
+      throw new Exception('The message is to long. (A maximum of '.Comments::option('max_character_count').' characters is allowed.)', 309);
     }
     
     $this->content_page = $content_page;
