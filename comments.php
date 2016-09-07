e0f4aaaaeb7ae7b90b5b7244eaea83892c59de4b
Florian Pircher <git@leuchtschirm.com>
2016-09-07 22:59:09 +0200

    Register blueprints and snippets in the registry

--- comments.php
+++ comments.php
@@ -5,6 +5,13 @@ include_once('plugin/comments-email.php');
 include_once('plugin/comments-status.php');
 
 /**
+ * The Kirby extension registry
+ */
+$kirby->set('blueprint', 'comment',  __DIR__ . '/blueprints/comment.php');
+$kirby->set('blueprint', 'comments', __DIR__ . '/blueprints/comments.php');
+$kirby->set('snippet',   'comments', __DIR__ . '/snippets/comments.php');
+
+/**
  * Comments
  */
 class Comments implements Iterator
