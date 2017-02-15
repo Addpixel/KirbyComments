634b494856444046a9bad29c59aafbc4b987b4b2
Florian Pircher <florian@addpixel.net>
2017-02-15 11:25:18 +0100

    Add introduction in comments snippet

--- snippets/comments.php
+++ snippets/comments.php
@@ -1,6 +1,20 @@
-<?php $comments = new Comments($page) ?>
-<?php $status = $comments->process() ?>
+<?php
 
+/*
+ * This is the example comments snippet. Feel free to use this code as a
+ * reference for creating your own, custom comments snippet.
+ * 
+ * Custom snipper markup guide:
+ * <https://github.com/Addpixel/KirbyComments#custom-markup>
+ * 
+ * API documentation:
+ * <https://github.com/Addpixel/KirbyComments#api-documentation>
+ */
+
+$comments = new Comments($page);
+$status = $comments->process();
+
+?>
 <?php if (!$comments->isEmpty()): ?>
   <h2>Comments</h2>
   
