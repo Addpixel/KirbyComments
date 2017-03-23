e5b1d3cb6700643f92b155983a7755e16adb52e3
Florian Pircher <florian@addpixel.net>
2017-03-23 23:04:38 +0100

    Update extension registry references to blueprints

--- comments.php
+++ comments.php
@@ -8,6 +8,6 @@ include_once('plugin/comments-status.php');
 /**
  * The Kirby extension registry
  */
-$kirby->set('blueprint', 'comments', __DIR__ . '/blueprints/comments.php');
-$kirby->set('blueprint', 'comment',  __DIR__ . '/blueprints/comment.php');
+$kirby->set('blueprint', 'comments', __DIR__ . '/blueprints/comments.yml');
+$kirby->set('blueprint', 'comment',  __DIR__ . '/blueprints/comment.yml');
 $kirby->set('snippet',   'comments', __DIR__ . '/snippets/comments.php');
