d380011872a5d57c9df956e073d569388f303959
Florian Pircher <florian@addpixel.net>
2017-05-25 13:02:26 +0200

    Add `$page->comments()` page methods extension

--- comments.php
+++ comments.php
@@ -5,8 +5,15 @@ include_once('plugin/comment.php');
 include_once('plugin/comments-email.php');
 include_once('plugin/comments-status.php');
 
-/**
- * The Kirby extension registry
+/*
+ * Custom page methods
+ */
+page::$methods['comments'] = function ($page) {
+  return Comments::for_page($page);
+};
+
+/*
+ * Kirby extension registry
  */
 $kirby->set('blueprint', 'comments', __DIR__.'/blueprints/comments.yml');
 $kirby->set('blueprint', 'comment', __DIR__.'/blueprints/comment.yml');
